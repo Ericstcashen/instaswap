84a00ad56ef5efe06d8567371f594ce13766b2cc
Eric <ericstcashen@proton.me>
2026-07-03 14:47:34 -0700

    iOS: Capacitor app scaffold, app icon/splash, camera perms, builds + runs in Simulator

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ericstcashen.instaswap',
+  appName: 'InstaSwap',
+  webDir: 'public'
+};
+
+export default config;
